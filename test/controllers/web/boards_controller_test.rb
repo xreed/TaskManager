8741c551f35dfbce8db68711a007b52fcde6fde8
Yakov <yakov.abdreimov@gmail.com>
2019-06-19 19:30:03 +0400

    authentication for board

--- test/controllers/web/boards_controller_test.rb
+++ test/controllers/web/boards_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class Web::BoardsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    user = create(:user)
+    sign_in_as user
+  end
+
   test "should get show" do
     get board_url
     assert_response :success
